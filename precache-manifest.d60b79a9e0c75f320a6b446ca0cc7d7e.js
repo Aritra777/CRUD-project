a025747c541f43752d1c194b4fd83b7040f7e55b
Aritra Biswas <93366359+Aritra777@users.noreply.github.com>
2022-03-13 11:34:30 +0530

    Files Uploaded

---
+++ precache-manifest.d60b79a9e0c75f320a6b446ca0cc7d7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c766fbf8165875aedf9f8a8773aa52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82d2fc6dca66f111555d",
+    "url": "/static/css/main.621ba682.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d2fc6dca66f111555d",
+    "url": "/static/js/main.a4606752.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
